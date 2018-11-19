1514cc4a01bda60bc1187ef8663be7398f950536
Patrick Louie Policarpio <patricklouie.pl@gmail.com>
2018-11-19 10:49:36 +0800

    Initial Commit.

--- cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
+++ cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -1,35 +1,38 @@
 package com.template
 
-import net.corda.core.contracts.CommandData
-import net.corda.core.contracts.Contract
-import net.corda.core.contracts.ContractState
-import net.corda.core.identity.AbstractParty
+import net.corda.core.contracts.*
+import net.corda.core.identity.Party
 import net.corda.core.transactions.LedgerTransaction
 
 // ************
 // * Contract *
 // ************
-class TemplateContract : Contract {
+class UserContract : Contract {
     companion object {
         // Used to identify our contract when building a transaction.
-        const val ID = "com.template.TemplateContract"
-    }
-    
-    // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
-    // does not throw an exception.
-    override fun verify(tx: LedgerTransaction) {
-        // Verification logic goes here.
+        const val USER_CONTRACT_ID = "com.template.UserContract"
     }
 
-    // Used to indicate the transaction's intent.
     interface Commands : CommandData {
-        class Action : Commands
+        class Register : Commands
+        class Update : Commands
+    }
+
+    override fun verify(tx: LedgerTransaction) {
+
     }
 }
 
 // *********
 // * State *
 // *********
-data class TemplateState(val data: String) : ContractState {
-    override val participants: List<AbstractParty> = listOf()
+data class UserState(val owningNode: Party,
+                     val name: String,
+                     val age: Int,
+                     val address: String,
+                     val birthDate: String,
+                     val status: String,
+                     val religion: String,
+                     val isVerified: Boolean) : ContractState {
+    override val participants = listOf(owningNode)
 }
--- cordapp/src/main/kotlin/com/template/Flows.kt
+++ cordapp/src/main/kotlin/com/template/Flows.kt
@@ -1,7 +1,14 @@
 package com.template
 
 import co.paralleluniverse.fibers.Suspendable
+import com.template.UserContract.Companion.USER_CONTRACT_ID
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.flows.*
+import net.corda.core.identity.Party
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
 
 // *********
@@ -9,19 +16,137 @@ import net.corda.core.utilities.ProgressTracker
 // *********
 @InitiatingFlow
 @StartableByRPC
-class Initiator : FlowLogic<Unit>() {
-    override val progressTracker = ProgressTracker()
+class RegisterFlow(val owningNode: Party,
+                   val name: String,
+                   val age: Int,
+                   val address: String,
+                   val birthDate: String,
+                   val status: String,
+                   val religion: String,
+                   val isVerified: Boolean) : FlowLogic<Unit>() {
+
+    /* Declare Transaction Steps */
+    companion object {
+        object BUILDING_TRANSACTION : ProgressTracker.Step("Building Transaction")
+        object SIGN_TRANSACTION : ProgressTracker.Step("Signing Transaction")
+        object VERIFY_TRANSACTION : ProgressTracker.Step("Verifying Transaction")
+        object NOTARIZE_TRANSACTION : ProgressTracker.Step("Notarizing Transaction")
+        object RECORD_TRANSACTION : ProgressTracker.Step("Recording Transaction")
+    }
+
+    fun tracker() = ProgressTracker(
+            BUILDING_TRANSACTION,
+            SIGN_TRANSACTION,
+            VERIFY_TRANSACTION,
+            NOTARIZE_TRANSACTION,
+            RECORD_TRANSACTION
+    )
+
+    override val progressTracker = tracker()
 
     @Suspendable
     override fun call() {
-        // Initiator flow logic goes here.
+
+        /* Step 1 - Build the transaction */
+        progressTracker.currentStep = BUILDING_TRANSACTION
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+        val outputState = UserState(
+                owningNode,
+                name,
+                age,
+                address,
+                birthDate,
+                status,
+                religion,
+                isVerified)
+        val cmd = Command(UserContract.Commands.Register(), owningNode.owningKey)
+
+        val txBuilder = TransactionBuilder(notary)
+                .addOutputState(outputState, USER_CONTRACT_ID)
+                .addCommand(cmd)
+
+        /* Step 2 - Sign the transaction */
+        progressTracker.currentStep = SIGN_TRANSACTION
+        val signedTx = serviceHub.signInitialTransaction(txBuilder)
+
+        /* Step 3 - Verify the transaction */
+        progressTracker.currentStep = VERIFY_TRANSACTION
+        signedTx.verify(serviceHub)
+
+        /* Step 4 and 5 - Notarize then Record the transaction */
+        progressTracker.currentStep = NOTARIZE_TRANSACTION
+        progressTracker.currentStep = RECORD_TRANSACTION
+        subFlow(FinalityFlow(signedTx))
     }
 }
 
-@InitiatedBy(Initiator::class)
-class Responder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
+@InitiatingFlow
+@StartableByRPC
+class UpdateFlow(val owningNode: Party,
+                 val name: String,
+                 val age: Int,
+                 val address: String,
+                 val birthDate: String,
+                 val status: String,
+                 val religion: String,
+                 val isVerified: Boolean) : FlowLogic<Unit>() {
+
+    /* Declare Transaction Steps */
+    companion object {
+        object BUILDING_TRANSACTION : ProgressTracker.Step("Building Transaction")
+        object SIGN_TRANSACTION : ProgressTracker.Step("Signing Transaction")
+        object VERIFY_TRANSACTION : ProgressTracker.Step("Verifying Transaction")
+        object NOTARIZE_TRANSACTION : ProgressTracker.Step("Notarizing Transaction")
+        object RECORD_TRANSACTION : ProgressTracker.Step("Recording Transaction")
+    }
+
+    fun tracker() = ProgressTracker(
+            BUILDING_TRANSACTION,
+            SIGN_TRANSACTION,
+            VERIFY_TRANSACTION,
+            NOTARIZE_TRANSACTION,
+            RECORD_TRANSACTION
+    )
+
+    override val progressTracker = tracker()
+
     @Suspendable
     override fun call() {
-        // Responder flow logic goes here.
+
+        /* Step 1 - Build the transaction */
+        progressTracker.currentStep = BUILDING_TRANSACTION
+        val inputCriteria = QueryCriteria.VaultQueryCriteria()
+        val inputStateAndRef = serviceHub.vaultService.queryBy<UserState>(inputCriteria).states.first()
+//        val input = inputStateAndRef.state.data
+
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+        val outputState = UserState(
+                owningNode,
+                name,
+                age,
+                address,
+                birthDate,
+                status,
+                religion,
+                isVerified)
+        val cmd = Command(UserContract.Commands.Register(), owningNode.owningKey)
+
+        val txBuilder = TransactionBuilder(notary)
+                .addInputState(inputStateAndRef)
+                .addOutputState(outputState, USER_CONTRACT_ID)
+                .addCommand(cmd)
+
+        /* Step 2 - Sign the transaction */
+        progressTracker.currentStep = SIGN_TRANSACTION
+        val signedTx = serviceHub.signInitialTransaction(txBuilder)
+
+        /* Step 3 - Verify the transaction */
+        progressTracker.currentStep = VERIFY_TRANSACTION
+        signedTx.verify(serviceHub)
+
+        /* Step 4 and 5 - Notarize then Record the transaction */
+        progressTracker.currentStep = NOTARIZE_TRANSACTION
+        progressTracker.currentStep = RECORD_TRANSACTION
+        subFlow(FinalityFlow(signedTx))
     }
 }
