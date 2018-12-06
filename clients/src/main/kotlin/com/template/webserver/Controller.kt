68663ad4431e2f068d691c8f2e03b2b6e18d72f3
Christian <christiandismaya.mvp@gmail.com>
2018-12-06 13:24:16 +0800

    updated API

--- clients/src/main/kotlin/com/template/webserver/Controller.kt
+++ clients/src/main/kotlin/com/template/webserver/Controller.kt
@@ -1,65 +1,63 @@
 package com.template.webserver
 
 import net.corda.core.contracts.ContractState
+import net.corda.core.contracts.StateAndRef
 import net.corda.core.messaging.vaultQueryBy
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
+import net.corda.core.node.services.Vault
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.utilities.loggerFor
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 import java.time.ZoneId
 
-
-private const val CONTROLLER_NAME = "config.controller.name"
-/**
- *  A controller for interacting with the node via RPC.
- */
 /**
  * Define your API endpoints here.
  */
 @RestController
 @RequestMapping("/api") // The paths for HTTP requests are relative to this base path.
-class Controller(
-        rpc: NodeRPCConnection,
-        @Value("\${$CONTROLLER_NAME}") private val controllerName: String ) {
+class Controller( rpc: NodeRPCConnection){
 
-    companion object {
-        private val logger = LoggerFactory.getLogger(RestController::class.java)
-    }
-    private val myName = rpc.proxy.nodeInfo().legalIdentities.first().name
-
+    private val myIdentity = rpc.proxy.nodeInfo().legalIdentities.first().name
     private val proxy = rpc.proxy
 
-    /** Returns the node's name. */
-    @GetMapping(value = "/myname", produces = arrayOf("text/plain"))
-    private fun myName() = myName.toString()
+    companion object {
+        private val logger = loggerFor<Controller>()
+    }
 
-    @GetMapping(value = "/status", produces = arrayOf("text/plain"))
+    /** Returns the node's name. */
+    @GetMapping(value = "/myname", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    private fun myName() = myIdentity.toString()
+
+    @GetMapping(value = "/status", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     private fun status() = "200"
 
-    @GetMapping(value = "/servertime", produces = arrayOf("text/plain"))
+    @GetMapping(value = "/servertime", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     private fun serverTime() = LocalDateTime.ofInstant(proxy.currentNodeTime(), ZoneId.of("UTC")).toString()
 
-    @GetMapping(value = "/addresses", produces = arrayOf("text/plain"))
+    @GetMapping(value = "/addresses", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     private fun addresses() = proxy.nodeInfo().addresses.toString()
 
-    @GetMapping(value = "/identities", produces = arrayOf("text/plain"))
+    @GetMapping(value = "/identities", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     private fun identities() = proxy.nodeInfo().legalIdentities.toString()
 
-    @GetMapping(value = "/platformversion", produces = arrayOf("text/plain"))
+    @GetMapping(value = "/platformversion", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     private fun platformVersion() = proxy.nodeInfo().platformVersion.toString()
 
-    @GetMapping(value = "/peers", produces = arrayOf("text/plain"))
+    @GetMapping(value = "/peers", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     private fun peers() = proxy.networkMapSnapshot().flatMap { it.legalIdentities }.toString()
 
-    @GetMapping(value = "/notaries", produces = arrayOf("text/plain"))
+    @GetMapping(value = "/notaries", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     private fun notaries() = proxy.notaryIdentities().toString()
 
-    @GetMapping(value = "/flows", produces = arrayOf("text/plain"))
+    @GetMapping(value = "/flows", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     private fun flows() = proxy.registeredFlows().toString()
 
-    @GetMapping(value = "/states", produces = arrayOf("text/plain"))
+    @GetMapping(value = "/states", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     private fun states() = proxy.vaultQueryBy<ContractState>().states.toString()
 
-}
+    @GetMapping(value = "/users", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    private fun users() = proxy.vaultQueryBy<ContractState>(QueryCriteria.VaultQueryCriteria()).states.toString()
+    }
